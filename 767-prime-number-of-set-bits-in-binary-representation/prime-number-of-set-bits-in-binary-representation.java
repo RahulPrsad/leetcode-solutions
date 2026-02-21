694ba6b3253696d00d12e951a4f2dcc123a7d2f1
Rahul Prasad <rahulprasad4968@gmail.com>
2026-02-21 23:39:39 +0530

    Time: 7 ms (49.72%) | Memory: 42.4 MB (44.84%) - LeetSync

---
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        int res = 0;
+
+        for (int i = left; i <= right; i++) {
+            int bitCount = countbit(i);
+
+            if (isPrime(bitCount)) {
+                res++;
+            }
+        }
+
+        return res;
+    }
+
+    public int countbit(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1); 
+            count++;
+        }
+        return count;
+    }
+
+    public boolean isPrime(int n) {
+        if (n < 2) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
